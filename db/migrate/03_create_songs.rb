06184d40eff57d7077bed455634156ae13ece7df
Andrew Waller <andrewhwaller@gmail.com>
2019-04-18 20:55:57 +0000

    Automatically backed up by Learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -3,7 +3,7 @@ class CreateSongs < ActiveRecord::Migration[4.2]
     create_table :songs do |s|
       s.string :name
       s.integer :artist_id
-      s,integer :genre_id
+      s.integer :genre_id
     end
   end
 end
